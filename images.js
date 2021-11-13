6e7c41f81bf5135ca21e5d2a822f85c9aa747b2a
Zahirudeen Premji <nondescript74@gmail.com>
2021-11-13 14:19:32 -0700

    with images.html and images.js working

--- images.js
+++ images.js
@@ -1,38 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   // Select the submit button and input to be used later
-  const searchField = document.querySelector('#searchField');
+  const searchField = document.querySelector('#googleSearchImg');
+  const googleSearch = document.querySelector('#button');
+
 
   // Disable submit button by default:
   googleSearch.disabled = true;
-  luckySearch.disabled = true;
 
   // Listen for input to be typed into the input field
   searchField.onkeyup = () => {
     if (searchField.value.length > 0) {
       googleSearch.disabled = false;
-      luckySearch.disabled = false;
     }
     else {
       googleSearch.disabled = true;
-      luckySearch.disabled = true;
     }
   }
 
-  document.querySelector('#searchField').onclick = () => {
+  document.querySelector('#button').onclick = () => {
     const searchThis = searchField.value;
-    console.log('luckySearch: ' + searchThis);
-    const wikipedia = 'https://en.wikipedia.org/wiki/';
-    const url = wikipedia + searchThis;
-    console.log('luckySearch url: ' + url);
+    console.log('googleSearch: ' + searchThis);
+    const google = 'https://www.google.com/images?q=';
+    const url = google + searchThis;
+    console.log('googleSearch url: ' + url);
 
     searchField.value = '';
 
     googleSearch.disabled = true;
-    luckySearch.disabled = true;
+
     const win = window.open(url, '_blank');
     win.focus();
-    return false;
     return false;
   }
 });
